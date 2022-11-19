a0f77ecea8d994d5978c5e940b07f94983298840
Julian Lopez <julianlopez6850@gmail.com>
2022-11-18 22:53:24 -0500

    Working Prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import Select from 'react-select';
 import './App.css';
 
-function App() {
+import { Credentials } from './Credentials';
+import Artists from './Artists';
+import Main from './pages/Main';
+
+const artistIndex = Math.floor(Math.random() * Artists.length);
+const chosenArtist = Artists[artistIndex];
+
+const App = () => {
+
+  const [token, setToken] = useState("");
+  const [albums, setAlbums] = useState({ selectedAlbum: "", listOfAlbums: []});
+  const [track, setTrack] = useState({ selectedTrack: "", listOfTracks: []})
+  const [trackImg, setTrackImg] = useState("");
+  const [guess, setGuess] = useState("");
+  const [numGuesses, setNumGuesses] = useState(0);
+  const [prevGuesses, setPrevGuesses] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+
+  useEffect (() => {
+
+    axios('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(Credentials().ClientID + ':' + Credentials().ClientSecret)
+      },
+      data: 'grant_type=client_credentials'
+    })
+    .then(tokenResponse => {
+      setToken(tokenResponse.data.access_token);
+
+      axios(`https://api.spotify.com/v1/artists/${chosenArtist.value}/albums?limit=10`, {
+        method: 'GET',
+        headers: {
+          'Authorization' : 'Bearer ' + tokenResponse.data.access_token
+        }
+      })
+      .then(albumResponse => {
+        setAlbums({
+          selectedAlbum: albumResponse.data.items[Math.floor(Math.random() * albumResponse.data.items.length)],
+          listOfAlbums: albumResponse.data.items
+        })
+      });
+    })
+  }, [])
+
+  // 5zi7WsKlIiUXv09tbGLKsE
+
+  // By artist: https://api.spotify.com/v1/search?type=track&q=artist:Michael Jackson
+
+  // By playlist: https://api.spotify.com/v1/playlists/3RFP1W7BAxOr6wfpoEusO7/tracks
+
+  useEffect(() => {
+    if(albums.selectedAlbum !== "")
+    {
+      axios(`https://api.spotify.com/v1/albums/${albums.selectedAlbum.id}/tracks`, {
+          method: 'GET',
+          headers: {
+            'Authorization' : 'Bearer ' + token
+          }
+        })
+        .then(trackResponse => {
+          setTrack({
+            selectedTrack: trackResponse.data.items[Math.floor(Math.random() * trackResponse.data.items.length)],
+            listOfTracks: trackResponse.data.items
+          })
+        });
+    }
+  }, [albums])
+
+  useEffect(() => {
+    if(track.selectedTrack !== "")
+    {
+      axios(`https://api.spotify.com/v1/tracks/${track.selectedTrack.id}`, {
+          method: 'GET',
+          headers: {
+            'Authorization' : 'Bearer ' + token
+          }
+        })
+        .then(trackResponse => {
+          console.log(trackResponse.data.album.name)
+          setTrackImg(trackResponse.data.album.images[1].url)
+        });
+    }
+  }, [track])
+
+  const checkGuess = () => {
+    setNumGuesses(numGuesses => numGuesses + 1);
+  }
+
+  useEffect(() => {
+    if(numGuesses > 0 && !gameOver)
+    {
+      console.log(guess.label);
+      console.log(chosenArtist.label);
+      console.log(numGuesses);
+
+      if(guess.label === chosenArtist.label)
+      {
+        console.log("YOU WON!");
+        setGameOver(true);
+      }
+      else if(numGuesses >= 6)
+      {
+        console.log("YOU LOST.");
+        setGameOver(true);
+      }
+
+      setPrevGuesses(prevGuesses => [...prevGuesses, guess]);
+  }
+  }, [numGuesses])
+
+  useEffect(() => {
+    console.log(prevGuesses);
+  }, [prevGuesses])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
+      <div className="albumArt" >
+        <img src={trackImg}  id={(gameOver) ? "end" : ("guess" + numGuesses)} />
+      </div>
+      <div className="guess">
+        <Select options={Artists} onChange={(selection) => {setGuess(selection)}} />
+        <button onClick={checkGuess}>
+          GUESS
+        </button>
+      </div>
+      <br />
+      <div>
+        {
+          prevGuesses.map((item, index) => <div>Guess {index + 1} : {item.label}</div>)
+        }
+      </div>
     </div>
   );
 }
---
+++ src/pages/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../styles/main.css";
+
+const Main = (props) => {
+
+	return (
+		<div>
+			<div className="title">
+				ALBUMLE
+			</div>
+			<div className="subtitle">
+				GUESS THE ALBUM FROM ITS ART
+			</div>
+		</div>
+	);
+}
+  
+export default Main;
