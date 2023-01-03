a25c845084450a0e82821a9286bc21e00395f52c
Julian Lopez <julianlopez6850@gmail.com>
2023-01-03 01:05:46 -0500

    Refactored Statistics modal
    - Removed repeated code, improved code readability, & replaced html elements with Chakra UI components.

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -10,6 +10,10 @@ import {
 	ModalBody,
 	ModalCloseButton,
 	Progress,
+  HStack,
+  VStack,
+  Text,
+  Box,
 } from '@chakra-ui/react'
 
 export const getStats = async (e, loggedIn, username, mode, setStats, onOpen) => {
@@ -48,78 +52,81 @@ export const getStats = async (e, loggedIn, username, mode, setStats, onOpen) =>
 const Statistics = (props) => {
 
   return (
-    <div>
-      <Modal
+    <Modal
       isCentered
       onClose={props.onClose}
       isOpen={props.isOpen}
       motionPreset='slideInBottom'
-      >
-        <ModalOverlay />
-        <ModalContent 
+    >
+      <ModalOverlay />
+      <ModalContent 
         color="white"
-        bgColor="gray.700">
-          <ModalHeader style={{display:"flex", justifyContent:"center" }}>{props.mode ? `${props.mode} Mode` : ``}  Statistics</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <div style={{display:"flex", flexDirection:"row", justifyContent:"space-evenly", textAlign:"center"}}>
-              <div style={{display:"flex", flexDirection:"column", minWidth:65, width:"fit-content", alignItems:"center"}}>
-                <div style={{fontSize:36, fontWeight:"bold"}}>{props.stats.numGames}</div>
-                <div style={{width:65, marginTop:-10}}>Games Played</div>
-              </div>
-              <div style={{display:"flex", flexDirection:"column", minWidth:65, width:"fit-content", alignItems:"center"}}>
-                <div style={{fontSize:36, fontWeight:"bold"}}>{props.stats.numWins}</div>
-                <div style={{width:65, marginTop:-10}}>Wins</div>
-              </div>
-              <div style={{display:"flex", flexDirection:"column", minWidth:65, width:"fit-content", alignItems:"center"}}>
-                <div style={{fontSize:36, fontWeight:"bold"}}>{props.stats.numLosses}</div>
-                <div style={{width:65, marginTop:-10}}>Losses</div>
-              </div>
-              <div style={{display:"flex", flexDirection:"column", minWidth:65, width:"fit-content", alignItems:"center"}}>
-                <div style={{fontSize:36, fontWeight:"bold"}}>{props.stats.winPercent}</div>
-                <div style={{width:65, marginTop:-10}}>Win %</div>
-              </div>
-              <div style={{display:"flex", flexDirection:"column", minWidth:65, width:"fit-content", alignItems:"center"}}>
-                <div style={{fontSize:36, fontWeight:"bold"}}>{props.stats.avgGuessesPerWin}</div>
-                <div style={{width:65, marginTop:-10}}>Avg Guesses</div>
-              </div>
-            </div>
+        bgColor="gray.700"
+      >
+        <ModalHeader
+          display="flex"
+          justifyContent="center"
+        >
+          {props.mode ? `${props.mode} Mode` : ``}  Statistics
+        </ModalHeader>
 
-            <br/>
-            <strong style={{ display:"flex", justifyContent:"center" }}>Guess Distribution:</strong>
+        <ModalCloseButton />
 
-            {/* Display progress bars and # of each item for guess distribution */}
-            {(props.stats.guessDistribution) ? [...props.stats.guessDistribution, props.stats.numLosses].map((item, index) => {
-              return (
-                <div key={index}>
-                  {(index===6) ? "Loss:" : (index + 1) + " Guess Win:"}
-                  <Progress
-                    style={{ zIndex:"-1" }}
-                    size='lg'
-                    colorScheme={(index===6) ? 'red' : 'green'}
-                    bgColor="gray.700"
-                    value={Math.max((item / props.stats.mostFrequent) * 100, 5)}
-                  />
-                  <div
-                    style={{
-                      margin:"-21px 0px 0px 5px",
-                      textAlign:"left"
-                    }}
-                  >
-                    {item}
-                  </div>
-                </div>
-            )}) : ""}
-            
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={props.onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </div>
+        <ModalBody>
+
+          {/* Display basic stats */}
+          <HStack align="top" justifyContent="space-evenly" textAlign="center">
+            {[{title: 'Games Played', stat: props.stats.numGames},
+              {title: 'Wins', stat: props.stats.numWins},
+              {title: 'Losses', stat: props.stats.numLosses},
+              {title: 'Win %', stat: props.stats.winPercent},
+              {title: 'Avg Guesses', stat: props.stats.avgGuessesPerWin}
+            ].map((item, index) => 
+              <VStack key={index} minWidth="65px" width="fit-content" spacing="-2">
+                <Text fontSize="36" fontWeight="bold">
+                  {item.stat}
+                </Text>
+                <Text width="65px">
+                  {item.title}
+                </Text>
+              </VStack>
+            )}
+          </HStack>
+
+          <br/>
+
+          {/* Display Guess distribution */}
+          <Text display="flex" justifyContent="center" fontWeight="bold">
+            Guess Distribution:
+          </Text>
+          {(props.stats.guessDistribution) ? [...props.stats.guessDistribution, props.stats.numLosses].map((item, index) => {
+            return (
+              <Box key={index}>
+                {(index===6) ? "Loss:" : (index + 1) + " Guess Win:"}
+                <Progress
+                  zIndex="-1"
+                  size='lg'
+                  colorScheme={(index===6) ? 'red' : 'green'}
+                  bgColor="gray.700"
+                  value={Math.max((item / props.stats.mostFrequent) * 100, 5)}
+                />
+                <Box
+                  m="-21px 0px 0px 5px"
+                  textAlign="left" 
+                >
+                  {item}
+                </Box>
+              </Box>
+          )}) : ""}
+          
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme='blue' mr={3} onClick={props.onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   )
 }
 
