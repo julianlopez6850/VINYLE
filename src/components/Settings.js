dfad56d6da3ff27a50d4538c6e5ca486a5eae0c2
Julian Lopez <julianlopez6850@gmail.com>
2023-01-20 21:29:28 -0500

    Refactored Difficulty Tabs in Settings Modal

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -23,7 +23,20 @@ import {
 
 const Settings = (props) => {
 
-  const [difficulty, setDifficulty] = useState({label: "normal", value: 0 });
+  const difficulties = [
+    { label: "Normal", value: 0, color: "green", tooltip: undefined }, 
+    { label: "Hard", value: 1, color: "yellow", tooltip: 
+      <><Text>Normal Mode</Text>
+      <Text>+ Grayscale</Text></>
+    }, 
+    { label: "Extreme", value: 2, color: "red", tooltip: 
+      <><Text>Hard Mode</Text>
+      <Text>+ Inverted Color</Text>
+      <Text>+ Random Rotation</Text></>
+    }
+  ];
+
+  const [difficulty, setDifficulty] = useState({ label: "Normal", value: 0, color: "green" });
 
   return (
     <Modal
@@ -68,44 +81,26 @@ const Settings = (props) => {
             <Text as='b' marginBlock="0 !important">
               Difficulty:
             </Text>
-            <Tabs colorScheme={(difficulty.label === "normal") ? "green" : (difficulty.label === "hard") ? "yellow" : "red"} borderBottom="transparent" defaultIndex={difficulty.value}>
+            <Tabs colorScheme={difficulty.color} borderBottom="transparent" defaultIndex={difficulty.value}>
               <TabList >
-                <Tab bg="none" onMouseDown={()=>{setDifficulty({ label: "normal", value: 0 })}}>
-                  Normal
-                </Tab>
-                <Tooltip
-                  hasArrow
-                  display="flex"
-                  bg='var(--background-color)'
-                  label={
-                    <VStack spacing="-1" fontWeight="bold">
-                      <Text>Normal Mode</Text>
-                      <Text>+ Grayscale</Text>
-                    </VStack>
-                  }
-                >
-                  <Tab bg="none" onMouseDown={()=>{setDifficulty({ label: "hard", value: 1 })}}>
-                      <Text>
-                        Hard
-                      </Text>
-                  </Tab>
-                </Tooltip>
-                <Tooltip
-                  hasArrow
-                  display="flex"
-                  bg='var(--background-color)'
-                  label={
-                    <VStack spacing="-1" fontWeight="bold">
-                      <Text>Hard Mode</Text>
-                      <Text>+ Inverted Color</Text>
-                      <Text>+ Random Rotation</Text>
-                    </VStack>
-                  }
-                >
-                  <Tab bg="none" onMouseDown={()=>{setDifficulty({ label: "extreme", value: 2 })}}>
-                      Extreme
-                  </Tab>
-                </Tooltip>
+                {difficulties.map((item) => 
+                  <Tooltip
+                    hasArrow
+                    display="flex"
+                    bg='var(--background-color)'
+                    label={
+                      <VStack spacing="-1" fontWeight="bold">
+                        {item.tooltip}
+                      </VStack>
+                    }
+                  >
+                    <Tab bg="none" onMouseDown={()=>{setDifficulty({ label: item.label, value: item.value, color: item.color })}}>
+                        <Text>
+                          {item.label}
+                        </Text>
+                    </Tab>
+                  </Tooltip>
+                )}
               </TabList>
             </Tabs>
 
